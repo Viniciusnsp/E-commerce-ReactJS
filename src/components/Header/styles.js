313262bc83fec499e17d95babbc113477c339e69
Vinicius Pereira <viniciusnspp@gmail.com>
2021-05-06 22:41:47 -0300

    Routes and home style

---
+++ src/components/Header/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const ShoppingBasketIcon = styled(MdShoppingBasket)`
+  width: 36px;
+  height: 36px;
+  color: #FFF;
+
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: filter .2s;
+
+  &:hover {
+    filter: brightness(0.7);
+  }
+
+  > div {
+    text-align: right;
+    margin-right: 10px;
+
+    > strong {
+      display: block;
+      color: #FFF;
+    }
+
+    > span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, ProductList } from './styles';
+
+function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img src="" alt="" />
+      </li>
+    </ProductList>
+  );
+}
+
+export default Home;
